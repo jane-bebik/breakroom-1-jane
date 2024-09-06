83577aaf40122005fb5d09d47c914b9b69897f19
jane-bebik <jane.bebik@gmail.com>
2024-09-06 10:54:01 -0400

    waaaaa

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        //first number
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        //sum code
+        double result = num1 - num2;
+
+        //result
+        System.out.println("The result of adding "  + result);
+
+        }
+    }
+}
